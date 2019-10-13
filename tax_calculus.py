8f7e2ed71ef682444b55a3c87e454588e7da00d5
SergeyLankevich <43179675+SergeyLankevich@users.noreply.github.com>
2019-10-13 21:37:28 +0700

    Update tax_calculus.py

--- tax_calculus.py
+++ tax_calculus.py
@@ -3,10 +3,10 @@ import math
 # Declaring tax rates of the USA:
 taxation_rate = [0.1, 0.15, 0.25, 0.28, 0.33, 0.35, 0.396]
 
-# Declaring taxable annual income in the USA divided into steps by tax rates for different social groups.
+# Declaring taxable annual income in USA divided in steps by tax rates for different social groups.
 single_subject = [9075, 36900, 89350, 186350, 405100, 406750, math.inf]
 couple = [18150, 73800, 148850, 226850, 405100, 457600, math.inf]
-single_parent = [12950, 49400, 127550, 206600, 405100, 432200, 432201, math.inf]
+single_parent = [12950, 49400, 127550, 206600, 405100, 432200, math.inf]
 
 # Calculating user's annual income
 annual_income = 0
@@ -17,8 +17,8 @@ for month in range(12):
 
 def tax_calculation(taxation_steps):
     """
-    :param taxation_steps: Taxable annual income in the USA divided into steps by tax rates.
-    :return: Total tax sum paid.
+    :param taxation_steps: Taxable annual income in USA divided in steps by tax rates.
+    :return: Amount of tax paid.
     """
     for i in taxation_steps:
         if i == annual_income or i > annual_income:
@@ -41,7 +41,8 @@ def tax_calculation(taxation_steps):
 category = int(input())
 if category == 1:
     tax_calculation(single_subject)
+    print(tax_calculation(single_subject))
 elif category == 2:
-    tax_calculation(couple)
+    print(tax_calculation(couple))
 elif category == 3:
-    tax_calculation(single_parent)
+    print(tax_calculation(single_parent))
