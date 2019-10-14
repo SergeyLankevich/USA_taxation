c88ee5396b6e1d76410850c6a277039d6fe9d2d5
MalakhovIlyaPavlovich <malakhov.ilya.pavlovich@yandex.ru>
2019-10-14 22:24:00 +0700

    Empty message

--- en_local.py
+++ en_local.py
@@ -1,6 +1,6 @@
 INPUT_INCOME = 'Input your income for '
 MONTH_LIST = ['January', 'February', 'March', 'April', 'May', 'June', 'Jule', 'August', 'September', 'October', 'November', 'December']
-INPUT_CORRECT_INCOME = 'Please input your annual income correctly (integer): '
+INPUT_CORRECT_INCOME = 'Please input your income correctly (integer): '
 INPUT_TAX_DEDUCTIONS = 'Input what your annual tax deductions are equal to: '
 INPUT_CORRECT_TAX_DEDUCTIONS = 'Please input your tax deductions correctly (integer): '
 CATEGORY_INPUT = 'Choose a category from the proposed:\n1. Single subject.\n2. Married couple.\n3. Single parent.'
